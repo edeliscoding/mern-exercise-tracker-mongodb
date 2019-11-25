c34b2c35a7855a7093ad4561f6c1429ac17c941d
edeliscoding <edelreichd@gmail.com>
2019-11-25 10:01:36 -0700

    Updates

---
+++ precache-manifest.da62899871b3ca59ebe1d09d842cdd5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4136125f1f6e78a63aa81364e9eaf0b6",
+    "url": "/mern-exercise-tracker-mongodb/index.html"
+  },
+  {
+    "revision": "7ce62139ebb3391deb14",
+    "url": "/mern-exercise-tracker-mongodb/static/css/2.89d568a6.chunk.css"
+  },
+  {
+    "revision": "7ce62139ebb3391deb14",
+    "url": "/mern-exercise-tracker-mongodb/static/js/2.4dfc0d50.chunk.js"
+  },
+  {
+    "revision": "8b21d308f686d1f70d3d",
+    "url": "/mern-exercise-tracker-mongodb/static/js/main.5a61a44d.chunk.js"
+  },
+  {
+    "revision": "013f260ba0e4029e6b89",
+    "url": "/mern-exercise-tracker-mongodb/static/js/runtime~main.41c79410.js"
+  }
+]);
